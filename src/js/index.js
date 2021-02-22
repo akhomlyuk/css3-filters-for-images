1c33dc57b1ce3e59239730f35e1e419369b466a0
Andrey <relicbs@gmail.com>
2021-02-22 10:36:55 +0300

    v0.2

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,15 @@ import * as mdb from 'mdb-ui-kit'
 import screenshot from 'image-screenshot'
 
 const img = document.getElementById('imgfilter')
-const loadImage = document.querySelector('#loadImage')
+const loadImage = document.getElementById('loadImage')
 
 loadImage.onclick = function () {
   screenshot(img).download()
 }
 
+screenshot(img).then((url) => {
+  console.log(url)
+})
 export default {
   mdb,
 }
